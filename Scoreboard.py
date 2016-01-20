92fafef82d8f189fa4143f79f186ee53697ac1f4
MCPlayer81 <romecivi34@gmail.com>
2016-01-20 16:44:06 -0500

    Adding Stuff

---
+++ Scoreboard.py
@@ -0,0 +1,35 @@
+f = open("scores.txt", "r")
+Q = file.readline(0)
+R = file.readline(1)
+S = file.readline(2)
+T = file.readline(3)
+U = file.readline(4)
+V = file.readline(5)
+W = file.readline(6)
+X = file.readline(7)
+Y = file.readline(8)
+Z = file.readline(9)
+I = file.readline(10)
+b = sorted([I,Z,Y,X,W,V,U,T,S,R,Q])
+A = b[10]
+B = b[9]
+C = b[8]
+D = b[7]
+E = b[6]
+F = b[5]
+G = b[4]
+H = b[3]
+I = b[2]
+J = b[1]
+K = b[0]
+print"!<!Top Scores!>!"
+print"1st",A
+print"2nd",B
+print"3rd",C
+print"4th",D
+print"5th",E
+print"6th",F
+print"7th",G
+print"8th",H
+print"9th",I
+print"10th",J
