76cdb705ff4ed17d7b9ec6087f0a5e5fbe94e3c3
MCPlayer81 <romecivi34@gmail.com>
2016-01-21 11:54:55 -0500

    Fixed It

--- Scoreboard.py
+++ Scoreboard.py
@@ -8,19 +8,9 @@ b = score[1]
 c = score[2]
 d = score[3]
 e = score[4]
-f = score[5]
-g = score[6]
-h = score[7]
-i = score[8]
-j = score[9]
 print"!<!Top Scores!>!"
 print"1st",a
 print"2nd",b
 print"3rd",c
 print"4th",d
 print"5th",e
-print"6th",f
-print"7th",g
-print"8th",h
-print"9th",i
-print"10th",j
