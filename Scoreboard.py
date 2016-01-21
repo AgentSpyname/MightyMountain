40d5c644c8d36b3f3a1a8bf7244d238575e7ea45
MCPlayer81 <romecivi34@gmail.com>
2016-01-21 10:03:55 -0500

    Adding Score

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,35 +1,27 @@
-f = open("scores.txt", "r")
-Q = file.readline(0)
-R = file.readline(1)
-S = file.readline(2)
-T = file.readline(3)
-U = file.readline(4)
-V = file.readline(5)
-W = file.readline(6)
-X = file.readline(7)
-Y = file.readline(8)
-Z = file.readline(9)
-I = file.readline(10)
-b = sorted([I,Z,Y,X,W,V,U,T,S,R,Q])
-A = b[10]
-B = b[9]
-C = b[8]
-D = b[7]
-E = b[6]
-F = b[5]
-G = b[4]
-H = b[3]
-I = b[2]
-J = b[1]
-K = b[0]
+score = []
+with open("scores.txt") as f:
+	for i, line in enumerate(f):
+		score.append(int(line))
+
+score.sort(reverse=True)
+a = score[0]
+b = score[1]
+c = score[2]
+d = score[3]
+e = score[4]
+f = score[5]
+g = score[6]
+h = score[7]
+i = score[8]
+j = score[9]
 print"!<!Top Scores!>!"
-print"1st",A
-print"2nd",B
-print"3rd",C
-print"4th",D
-print"5th",E
-print"6th",F
-print"7th",G
-print"8th",H
-print"9th",I
-print"10th",J
+print"1st",a
+print"2nd",b
+print"3rd",c
+print"4th",d
+print"5th",e
+print"6th",f
+print"7th",g
+print"8th",h
+print"9th",i
+print"10th",j
