bca7154db990f3618c8b00fbfc68fa870d91a9ed
Alexander Parsan <alexanderparsan28@gmail.com>
2016-01-24 16:13:11 -0500

    Added checking for OS tmusic.py

--- Modules/music.py
+++ Modules/music.py
@@ -1,3 +1,9 @@
 from platform import *
 
-print system()
+if system() == "Windows":
+	print "You are a Microsoft Device"
+if system() == "Darwin":
+	print "You are on a Apple/Darwin Device"
+if system() == "Linux":
+	print "You are on a Linux Device"
+
